8f8b4c9a75c224d64b422b3da97e59ef400ad3fb
harapeko <github@harapeko.me>
2018-09-06 02:54:00 +0900

    add web socketの接続と切断をコンソールに表示 done

--- app.js
+++ app.js
@@ -5,12 +5,21 @@ const app = express();
 // 一般的なHTTPの処理はexpressで行うため
 // https://teratail.com/questions/8894
 const http = require('http').Server(app);
+const io = require('socket.io')(http);
 const PORT = process.env.PORT || 3000;
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html')
 });
 
+io.on('connection', function(socket) {
+  console.log('connected');
+
+  socket.on('disconnect', function() {
+    console.log('user disconnected')
+  })
+});
+
 http.listen(PORT, function() {
-  console.log(`PORT: ${PORT}`)
+  console.log(`listeing on *:${PORT}`);
 })
